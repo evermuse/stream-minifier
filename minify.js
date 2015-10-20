5046ece1ad8778b49fe080d7e7fe2537830223f9
Bryan Alexander <bryanalexander@me.com>
2015-10-20 10:33:41 -1000

    added comments

--- minify.js
+++ minify.js
@@ -2,23 +2,31 @@ var fs = require('fs');
 var Transform = require('stream').Transform;
 var inherits = require('util').inherits;
 
+//grab the arguments from the command line
+
 var args = process.argv.slice(2);
+
+//extend the Transform class
 
 function MinifyStream(options) {
 
   Transform.call(this);
 
 }
-inherits(MinifyStream, Transform);
+inherits(MinifyStream, Transform); //short-form way of extending a class and complete inheritance
+
+//modifies the transform method on our version of the Class to peform our desired functionality
 
 MinifyStream.prototype._transform = function (chunk, enc, done) {
 
-  chunk = chunk.toString().replace(/\r?\n|\r/g, '');
+  chunk = chunk.toString().replace(/\r?\n|\r/g, ''); //does the minifying
 
   this.push(chunk);
   done();
 
 };
+
+//read the arguments from the command line if formatted correctly or offer usage help if not
 
 if (args[0] === '--input' && args[2] === '--output') {
 
